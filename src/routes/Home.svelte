c237b2fe9c2e711c79fb449bc9de8c05e394bb5f
czcx <106482372+Mpro256@users.noreply.github.com>
2024-08-12 08:37:33 -0400

    Update Home.svelte

--- src/routes/Home.svelte
+++ src/routes/Home.svelte
@@ -5,13 +5,12 @@
   let chooser: HTMLInputElement;
 </script>
 
-<h1 class="my-6 text-5xl md:text-9xl">RatRater 2</h1>
+<h1 class="my-6 text-5xl md:text-9xl">RatPoison</h1>
 <p class="my-2 flex-col lg:flex">
   <span>
-    RatRater is a tool that analyzes .jar files for known signs of infostealers (rats), common among
-    the Hypixel Skyblock community.
+    RatPoison is a Minecraft RAT Detector based off of RatRater2. 
   </span>
-  <span>Drop, paste, or...</span>
+  <span>Drag and Drop, paste, or...</span>
 </p>
 <input
   type="file"
@@ -25,6 +24,5 @@
 />
 <Button type="filled" on:click={() => chooser.click()}>Choose a file</Button>
 <p class="mt-auto">
-  Made by @KTibow - <a href="https://discord.gg/ARW8nnh5Kw" class="underline-hover">RR2 Discord</a>
-  - <a href="https://discord.gg/7sBMTv7AF3" class="underline-hover">Vanta</a>
+  Made by @Czcx using code from @KTibow
 </p>
